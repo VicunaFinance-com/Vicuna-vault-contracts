fe364105c2b4e08f968fba6679fa3565e1d308eb
Yberjon <nunnom1@gmail.com>
2025-01-23 10:37:13 +0100

    New router abi

--- scripts/configure-swapper.js
+++ scripts/configure-swapper.js
@@ -1,6 +1,6 @@
 import VelodromeRouterAbi from "../data/abi/VelodromeRouter.json";
 import BalancerVaultAbi from "../data/abi/BalancerVault.json";
-import AlgebraRouterAbi from "../data/abi/AlgebraRouterAbi.json";
+import AlgebraRouterAbi from "../data/abi/AlgebraSwapxRouterAbi.json";
 import { BEETS, EQUAL, EQUALIZER_ROUTER_O2, ALGEBRA_ROUTER, SWAPX_ROUTER_V2, UINT256_MAX, USDC, WRAPPED_NATIVE, WRAPPER_ETH, BALANCER_VAULT, BEETS_STAKED_SONIC, INT256_MAX, RINGING_STABLE_BEETS, PUT_A_RING_ON_IT, STACKED_SONIC_SYMPHONY, BRUSH, FSONIC, FIERY, ECO, GOGLZ, ANON, HEDGY, WHALE, THC, SWPX, SACRA, ATOLL, ATOLL_ETH, SONIC_ETH, SONIC_USD } from "./config-sonic";
 
 const hardhat = require("hardhat");
